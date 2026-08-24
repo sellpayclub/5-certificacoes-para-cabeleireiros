5b5741cf843aa4fea34e47fac1b995e1791a1103
danielpaixaog <117575434+sellpayclub@users.noreply.github.com>
2026-08-24 16:12:05 -0300

    Atualização do projeto via CodAí

--- pages/_app.js
+++ pages/_app.js
@@ -52,7 +52,7 @@ export default function App({ Component, pageProps }) {
       const script = document.createElement("script");
       script.id = id;
       script.text = content;
-      (id === "utmfy-pixel-global" || id === "pixel-adicional-global" ? document.body : document.head).appendChild(script);
+      (id === "utmfy-pixel-global" || id === "utm-capture-script" || id === "pixel-adicional-global" || id === "facebook-pixel-1921904921813013" ? document.body : document.head).appendChild(script);
     });
   }, []);
 
@@ -72,12 +72,7 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-      <script
-        id="facebook-pixel-1921904921813013"
-        dangerouslySetInnerHTML={{
-          __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,"script","https://connect.facebook.net/en_US/fbevents.js");fbq("init","1921904921813013");fbq("track","PageView");`,
-        }}
-      />
+
       <noscript>
         <img
           height="1"
