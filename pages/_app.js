d7a80d7b2c333fa6be15ba675026fca5b2d82a9e
danielpaixaog <117575434+sellpayclub@users.noreply.github.com>
2026-08-24 16:14:35 -0300

    Atualização do projeto via CodAí

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import "../styles/globals.css";
 
 const permanentPixels = [
@@ -72,6 +73,23 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <script
+          id="facebook-pixel-1921904921813013"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1921904921813013');
+fbq('track', 'PageView');`,
+          }}
+        />
+      </Head>
 
       <noscript>
         <img
