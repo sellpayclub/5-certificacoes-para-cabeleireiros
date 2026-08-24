610d4750be590d70c7aa98f02d8eeaf132ab3532
danielpaixaog <117575434+sellpayclub@users.noreply.github.com>
2026-08-24 16:10:47 -0300

    Atualização do projeto via CodAí

--- pages/_app.js
+++ pages/_app.js
@@ -70,5 +70,24 @@ export default function App({ Component, pageProps }) {
     };
   }, [router.events]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <script
+        id="facebook-pixel-1921904921813013"
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,"script","https://connect.facebook.net/en_US/fbevents.js");fbq("init","1921904921813013");fbq("track","PageView");`,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1921904921813013&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      <Component {...pageProps} />
+    </>
+  );
 }
